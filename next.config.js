cd94fd718dea13e5f670244ae8050a7dfa6ae337
Alper Aydin <alperaydin22@ku.edu.tr>
2026-04-15 03:11:01 +0300

    images fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    qualities: [75, 85, 90, 100],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
   },
 };
 
